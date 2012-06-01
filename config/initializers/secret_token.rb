7640ec3f8bdcea811b193c69f162b6051e2c0b7f
Tomohiko Himura <eiel.hal@gmail.com>
2012-06-01 14:57:07 +0900

    initial commit
    make dir oneline
    cd oneline
    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneline::Application.config.secret_token = '00100cff6a72c1f5cdcf2fd4ea2a5bb10adbd9c5cce0eb30899d497db9441d5bd5ea1cfbf8337fb24902e0d9af3cb91c4c8a00001fec8f38e13a6f9177e4b622'
