439fc9d3a0e65a42d0927e5e47f023dd1a5deff1
Tomohiko Himura <eiel.hal@gmail.com>
2012-06-01 16:06:47 +0900

    未定義だったステップを oneline_steps.rb に記述
    基本的に前回の `cucumber` の実行結果の最後に記述されてた部分をコピー&ペースト
    引数名などは調整した。
    実装後の `cucumber` の実行結果は `output.txt`に.

---
+++ features/step_definitions/oneline_steps.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+前提 /^"(.*?)"の"(.*?)"という投稿がある$/ do |name, comment|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"(.*?)"ページにアクセス$/ do |page_name|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"(.*?)"と表示されている$/ do |str|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"(.*?)"に"(.*?)"と入力$/ do |label, value|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"(.*?)"をクリック$/ do |str|
+  pending # express the regexp above with the code you wish you had
+end
