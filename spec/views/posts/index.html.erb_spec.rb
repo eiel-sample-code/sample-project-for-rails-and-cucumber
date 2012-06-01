d49a50ab262650f885747f1b0c5f246f5ad54b80
Tomohiko Himura <eiel.hal@gmail.com>
2012-06-01 19:17:36 +0900

    要求にあうようにビューの振舞を定義
    cucumberが失敗してる理由を参考にしながらかきます。

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 
 describe "posts/index" do
   subject { render }
+  before :each do
+    assign :post, Post.new
+  end
 
   context "投稿があるとき" do
     before :each do
@@ -11,6 +14,8 @@ describe "posts/index" do
     it { should have_css("#posts .name", text: "とうこうしゃ") }
     it { should have_css("#posts .comment", text: "こめんと") }
     it { should have_css("h1", text: "一行掲示板") }
+    it { should have_css("label", text: "名前") }
+    it { should have_css("label", text: "内容") }
   end
 
   context "投稿がないとき" do
@@ -19,5 +24,7 @@ describe "posts/index" do
     end
     it { should_not have_css(".post") }
     it { should have_css("h1", text: "一行掲示板") }
+    it { should have_css("label", text: "名前") }
+    it { should have_css("label", text: "内容") }
   end
 end
