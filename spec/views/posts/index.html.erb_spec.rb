2170b5341bd6ef9b18e2d71ed6e6f0ba714d5797
Tomohiko Himura <eiel.hal@gmail.com>
2012-06-01 18:39:42 +0900

    期待する動作になるような振舞いを定義
    フィーチャで定義しているステップがうまく動くようなviewの振舞を考えてコード化します。いきなり全部かかなくても良いと思いますが便宜上まとめてかきます。

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -3,5 +3,21 @@ require 'spec_helper'
 
 describe "posts/index" do
   subject { render }
-  it { should have_css("h1", text: "一行掲示板") }
+
+  context "投稿があるとき" do
+    before :each do
+      assign :posts, FactoryGirl.create_list(:post,1)
+    end
+    it { should have_css("#posts .name", text: "とうこうしゃ") }
+    it { should have_css("#posts .comment", text: "こめんと") }
+    it { should have_css("h1", text: "一行掲示板") }
+  end
+
+  context "投稿がないとき" do
+    before :each do
+      assign :posts, []
+    end
+    it { should_not have_css(".post") }
+    it { should have_css("h1", text: "一行掲示板") }
+  end
 end
